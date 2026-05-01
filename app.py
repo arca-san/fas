157fc758f2a86e2ddefcc9bb826ece2afa6d052e
soldanorta <arcasan4@gmail.com>
2026-05-01 17:38:59 +0300

    feat: dmc CSS stilleri uygulamaya eklendi

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ Dash uygulaması giriş noktası.
 
 import dash
 import dash_bootstrap_components as dbc
+import dash_mantine_components as dmc
 
 app = dash.Dash(
     __name__,
     use_pages=True,
-    external_stylesheets=[dbc.themes.FLATLY],
+    external_stylesheets=[dbc.themes.FLATLY, dmc.styles.ALL],
     suppress_callback_exceptions=True,
     title="Fon Analiz Sistemi",
 )
